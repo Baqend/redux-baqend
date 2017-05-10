b1ea733818a1dcd63267bc7564d90b1e78ae87c5
Julian Tiemann <jutiemann@gmail.com>
2017-05-10 17:49:09 +0200

    export redux action types

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { BAQEND_CONNECTING, BAQEND_CONNECTED } from './types';
 import baqendConnect from './connect';
 import baqendMiddleware from './middleware';
 import baqendReducer from './reducer';
 
 export {
+  BAQEND_CONNECTING,
+  BAQEND_CONNECTED,
   baqendConnect,
   baqendMiddleware,
   baqendReducer
