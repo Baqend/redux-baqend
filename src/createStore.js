82782e191008ea4a351d9c0835ccc6f1c9e8b6ef
Julian Tiemann <jutiemann@gmail.com>
2017-12-20 15:16:21 +0100

    add watch script

--- src/createStore.js
+++ src/createStore.js
@@ -3,6 +3,7 @@ import { createEnhancers } from './index'
 
 const createStoreWithBaqend = (db, reducer, initialState, middleware) => {
   const { baqendConnect, baqendMiddleware } = createEnhancers(db)
+  console.log("redux store yo")
   return createStore(reducer, initialState, compose(
     baqendConnect,
     applyMiddleware(baqendMiddleware),
